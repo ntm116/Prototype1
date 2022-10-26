113977ae479e092713ae2d7fae26fd9b5f179801
ntm116 <manhnt@metacity.email>
2022-10-26 12:01:26 +0700

    P1-2: Optimize 2
    ...

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 5f;
+    private float horsePower = 10000f;
 
     [SerializeField]
     private float turnSpeed;
@@ -16,6 +17,34 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float verticalInput;
 
+    [SerializeField]
+    private GameObject centerOfMass;
+
+    [SerializeField]
+    private TextMeshProUGUI speedometerText;
+
+    [SerializeField]
+    private TextMeshProUGUI rpmText;
+
+    [SerializeField]
+    private List<WheelCollider> allWheels;
+
+    private Rigidbody playerRb;
+
+    private float speed;
+    private float rpm;
+    [SerializeField]
+    private int wheelsOnTheGround;
+
+    private const float MPH_Coefficent = 2.237f;
+    private const float KPH_Coefficent = 3.6f;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -24,8 +53,34 @@ public class PlayerController : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // Move player
-        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * speed);
+        if (IsGrounded())
+        {
+            playerRb.AddRelativeForce(verticalInput * horsePower * Vector3.forward);
 
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        }
+    }
+
+    private void Update()
+    {
+        speed = Mathf.Round( playerRb.velocity.magnitude * MPH_Coefficent);
+        speedometerText.SetText("Speed: " + speed + "mph");
+
+        rpm = Mathf.Round(speed % 30 * 40);
+        rpmText.SetText("RPM: " + rpm);
+    }
+
+    bool IsGrounded()
+    {
+        wheelsOnTheGround = 0;
+        foreach(WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded) wheelsOnTheGround += 1;
+        }
+        
+
+        if (wheelsOnTheGround > 3)
+            return true;
+        return false;
     }
 }
